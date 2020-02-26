95b899ed3d8a4b550005f2446cd4e619ebcb82c8
Dennis Heutinck <dennis.heutinck@infocaster.net>
2020-02-26 08:35:08 +0100

    Add game with configuration.

---
+++ RayTracer/Game.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RayTracer
+{
+    public class Game
+    {
+        private Type startupType;
+        private IServiceCollection serviceCollection;
+
+        private Game(string[] args)
+        {
+            startupType = null;
+            serviceCollection = new ServiceCollection();
+
+            AddConfiguration(args);
+        }
+
+        public Game CreateDefaultGame(string[] args)
+        {
+            return new Game(args);
+        }
+
+        public Game UseStartup<T>() where T : class
+        {
+            serviceCollection.AddSingleton<T>();
+            startupType = typeof(T);
+            return this;
+        }
+
+        private void AddConfiguration(string[] args)
+        {
+            serviceCollection.AddSingleton<IConfigurationRoot>(
+                new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddCommandLine(args)
+                    .AddJsonFile("appsettings.json")
+                    .Build()
+            );
+        }
+    }
+}
